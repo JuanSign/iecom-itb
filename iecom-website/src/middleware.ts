6fccf5bea7f180117d89efdbda4538a94e9f97ac
Juan Sohuturon Arauna Siagian <jsa.siagian@gmail.com>
2025-11-15 18:46:32 +0700

    [fix] allow image request in middleware

--- iecom-website/src/middleware.ts
+++ iecom-website/src/middleware.ts
@@ -47,6 +47,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
